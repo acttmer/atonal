3a98571b538b81f6cd0dd4bbe3afca92790cf5a5
Aego <aego.yu@gmail.com>
2023-07-27 12:16:04 -0700

    improved zod

--- packages/atonal-http/src/router/schema.ts
+++ packages/atonal-http/src/router/schema.ts
@@ -1,11 +1,18 @@
 import {
   z as zod,
   type Primitive,
+  type RawCreateParams,
   type ZodLiteral,
   type ZodNever,
   type ZodTypeAny,
   type ZodUnion,
 } from 'zod'
+
+declare global {
+  interface ArrayConstructor {
+    isArray(arg: ReadonlyArray<any> | any): arg is ReadonlyArray<any>
+  }
+}
 
 type MappedZodLiterals<T extends readonly Primitive[]> = {
   -readonly [K in keyof T]: ZodLiteral<T[K]>
@@ -16,45 +23,56 @@ const createManyUnion = <
 >(
   literals: A,
 ) => {
-  return z.union(
+  return zod.union(
     literals.map(value => z.literal(value)) as MappedZodLiterals<A>,
   )
 }
 
-function createUnionSchema<T extends readonly []>(values: T): ZodNever
-function createUnionSchema<T extends readonly [Primitive]>(
+function createUnionSchema<const T extends Primitive>(values: T): ZodLiteral<T>
+function createUnionSchema<const T extends readonly []>(values: T): ZodNever
+function createUnionSchema<const T extends readonly [Primitive]>(
   values: T,
 ): ZodLiteral<T[0]>
 function createUnionSchema<
-  T extends readonly [Primitive, Primitive, ...Primitive[]],
+  const T extends readonly [Primitive, Primitive, ...Primitive[]],
 >(values: T): ZodUnion<MappedZodLiterals<T>>
-function createUnionSchema<T extends readonly Primitive[]>(values: T) {
+function createUnionSchema<const T extends readonly Primitive[] | Primitive>(
+  values: T,
+) {
+  if (!Array.isArray(values)) {
+    return zod.literal(values)
+  }
+
   if (values.length > 1) {
     return createManyUnion(
       values as typeof values & [Primitive, Primitive, ...Primitive[]],
     )
   } else if (values.length === 1) {
-    return z.literal(values[0])
+    return zod.literal(values[0])
   } else if (values.length === 0) {
-    return z.never()
+    return zod.never()
   }
 
   throw new Error('Array must have a length')
 }
 
-const createArrayFromStringSchema = <T extends ZodTypeAny>(schema: T) => {
-  return z.preprocess(obj => {
+const createArraySchema = <const T extends ZodTypeAny>(
+  schema: T,
+  params: RawCreateParams & { coerce?: boolean } = {},
+) => {
+  return zod.preprocess(obj => {
     if (Array.isArray(obj)) {
       return obj
-    } else if (typeof obj === 'string') {
+    } else if (params.coerce && typeof obj === 'string') {
       return obj.split(',')
     } else {
       return []
     }
-  }, z.array(schema))
+  }, zod.array(schema, params))
 }
 
-export const z = Object.assign(zod, {
-  literals: createUnionSchema,
-  arrayFromString: createArrayFromStringSchema,
-})
+export const z = {
+  ...zod,
+  literal: createUnionSchema,
+  array: createArraySchema,
+}
